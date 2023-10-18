fa9e998a92cb66f74fedc48fa8fa5f111f7706fa
Pdoyejide <oyejideprince@gmail.com>
2023-10-18 03:26:27 -0700

    updated printf project

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
 
 int simputchar(char c);
 int simputs(char *s);
@@ -17,5 +18,9 @@ void simrot13(char *str);
 void putbuff(char *buffer, const char *str, int *written, int max_length);
 void controlstr(const char *format, va_list args, int *count);
 int simprint_pointer(const char *format, ...);
-
+int sim_isdigit(int c);
+int sim_strlen(char *s);
+int sim_number(char *str, params_t *params);
+int sim_numb_right_shift(char *str, params_t *params);
+int sim_num_left_shift(char *str, params_t *params);
 #endif
